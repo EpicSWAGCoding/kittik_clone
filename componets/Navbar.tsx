88887877225cf4c70aa446768cb8ef1422d4c750
Егор <100159885+EpicSWAGCoding@users.noreply.github.com>
2022-07-27 09:47:00 +0300

    COMMIT_EDITMSG

--- componets/Navbar.tsx
+++ componets/Navbar.tsx
@@ -13,11 +13,11 @@ const Navbar = () => {
   return (
     <div className='w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4'>
       <Link href="/">
-        <div className='w-[100px] md:w-[130px]'>
+        <div className='w-[100px] md:w-[129px] md:h-[30px] h-[38px]'>
           <Image
             className='cursor-pointer'
             src={Logo}
-            alt="KitTik"
+            alt="logo"
             layout='responsive'
           />
         </div>
