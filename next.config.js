00356a6e64ffc35ddc73761212ce340514438f8d
Егор <100159885+EpicSWAGCoding@users.noreply.github.com>
2022-08-10 13:47:41 +0300

    first commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  images: {
+    domains: ['i.playground.ru',
+      'lh3.googleusercontent.com'
+    ],
+  }
 }
 
 module.exports = nextConfig
