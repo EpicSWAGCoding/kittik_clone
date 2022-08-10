5d646172cad6ed961b33786d11a49000b6b8dee2
Егор <100159885+EpicSWAGCoding@users.noreply.github.com>
2022-08-10 14:10:22 +0300

    change next config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  typescript: {
+    ignoreBuildErrors
+  },
+
   reactStrictMode: true,
   images: {
     domains: ['i.playground.ru',
